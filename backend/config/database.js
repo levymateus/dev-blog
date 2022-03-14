718f000ed176f7f3aaa522b191a7e615d105b551
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-03-14 17:20:03 -0300

    refactor(backend): some changes in schema and database
    uses postgres and some changes in schema

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5000),
+      database: env('DATABASE_NAME', 'db'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', 'admin'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    useNullAsDefault: true,
   },
 });
