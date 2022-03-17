23fe9ae181e9ae22fd87ffea5208925c32009216
Levy Mateus Macedo <lmateus_macedo@hotmail.com>
2022-03-17 09:29:00 -0300

    add ci/cd configs (#3)
    * refactor(yarn.lock): update yarn.lock
    
    * chore: change database port from 5000 to 5432
    
    * refactor(backend/config): change database default port and name
    
    * fix(backend/config): add missing env directory
    
    Co-authored-by: Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5000),
-      database: env('DATABASE_NAME', 'db'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'blog'),
       user: env('DATABASE_USERNAME', 'admin'),
       password: env('DATABASE_PASSWORD', 'admin'),
       ssl: env.bool('DATABASE_SSL', false),
