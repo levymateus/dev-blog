2972e916dfc6865bcfb6fb820a9fd26af92e32a0
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-03-17 08:48:57 -0300

    refactor(backend/config): change database default port and name

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db'),
+      database: env('DATABASE_NAME', 'blog'),
       user: env('DATABASE_USERNAME', 'admin'),
       password: env('DATABASE_PASSWORD', 'admin'),
       ssl: env.bool('DATABASE_SSL', false),
