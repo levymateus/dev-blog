2682d0ef0faf2cdf492674c2f382817109519772
Levy Mateus Macedo <lmateus_macedo@hotmail.com>
2022-03-17 17:55:21 -0300

    Add 404 page - not found (#6)
    * fix(frontend): default paths from aritlce[id] page
    
    * feat(pages/404): add 404 page
    
    * fix(usenotfound): add missing import
    
    Co-authored-by: Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>

--- frontend/pages/articles/[id].js
+++ frontend/pages/articles/[id].js
@@ -5,9 +5,13 @@ import { defaultOptions } from "../_app"
 import defaultPaths from "../../consts/paths"
 import { useContext } from "react"
 import GlobalContext from "../../contexts/global-context"
+import useNotFound from "../../hooks/useNotFound"
 
 function Article({ article }) {
   const global = useContext(GlobalContext)
+
+  useNotFound(() => !article)
+
   return <main className="articles">
     <Show when={Boolean(article)}>
       <Markdown code={global?.code.style} text={article?.attributes.text || ''} />
@@ -19,11 +23,9 @@ export async function getStaticPaths() {
   const queryClient = new QueryClient({ defaultOptions: defaultOptions })
   const { data: articles } = await queryClient.fetchQuery(['/articles'])
 
-  let paths = defaultPaths
-
-  paths = articles?.map(({ id }) => ({
+  const paths = articles?.map(({ id }) => ({
     params: { id: String(id) },
-  }))
+  })) || defaultPaths
 
   return { paths, fallback: false }
 }
