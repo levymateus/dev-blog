663e15b7935ca86278c0d59f8d35175830a0b778
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-03-23 10:47:53 -0300

    feat(frontend): add cms favicon

--- frontend/components/head.js
+++ frontend/components/head.js
@@ -1,12 +1,15 @@
 import Head from "next/head"
 import { useContext } from "react"
 import { GlobalContext } from "../contexts/global-context"
+import { getStrapiMedia } from "../lib/media"
 
 function AppHead() {
   const global = useContext(GlobalContext)
+  const iconUrl = getStrapiMedia(global.favicon.data.attributes)
   return <Head>
     <title>{global.title}</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="icon" href={iconUrl} />
   </Head>
 }
 
