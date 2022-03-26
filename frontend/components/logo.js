3ba61f0107489d15af76da7b4317b59de80aed22
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-03-25 21:42:44 -0300

    fix(frontend): logo props

--- frontend/components/logo.js
+++ frontend/components/logo.js
@@ -1,9 +1,20 @@
 import Image from "next/image"
 import { getStrapiMedia } from "../lib/media"
 
-function Logo({ url, width = 24, height = 24, caption }) {
+function Logo({ url, alt, width = 24, height = 24, caption }) {
   const src = getStrapiMedia({ url })
-  return src && <Image width={width} height={height} src={src} alt={caption} />
+
+  if(!src) {
+    return null
+  }
+
+  return <Image
+    width={width}
+    height={height}
+    src={src}
+    alt={alt}
+    caption={caption}
+  />
 }
 
 export default Logo
