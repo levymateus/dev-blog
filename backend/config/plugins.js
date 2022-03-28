38047500f076edbd3db0aa266ed271cb63734ac7
Levy Mateus Macedo <lmateus_macedo@hotmail.com>
2022-03-28 14:05:27 -0300

    feature: add file upload/hosting in aws s3 (#8)
    * feat(backend): add strapi aws s3 upload provider
    
    * feat(frontend): add aws s3 next.config image domain
    
    * chore(backend/yarn.lock): update yarn.lock

---
+++ backend/config/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env })=>({
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000
+        },
+      },
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
+});
