f6dd733cd84e46c2403f4474de17c473ef35da74
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-04-09 15:53:41 -0300

    chore: add bundle analyzer & update to react 18 & change react-markdown to markdown

--- frontend/components/markdown.js
+++ frontend/components/markdown.js
@@ -1,42 +1,17 @@
-import { Prism as SyntaxHighlighter} from "react-syntax-highlighter"
-import ReactMarkdown from "react-markdown"
-
-const code = ({ style }) => ({ node, inline, className, children, ...props }) => {
-  const match = /language-(\w+)/.exec(className || '')
-
-  return !inline && match ? (
-    <SyntaxHighlighter
-      children={String(children).replace(/\n$/, '')}
-      style={style}
-      language={match[1]}
-      PreTag="div"
-      {...props}
-    />
-  ) : (
-    <code className={className} {...props}>
-      {children}
-    </code>
-  )
-}
-
-const a = ({ node, children, ...props }) => {
-  const text = String(children).split('//')[0]
-  let jsonProps = {}
-  try {
-    jsonProps = JSON.parse(String(children).split('//')[1])
-    return <a
-      {...props}
-      {...jsonProps}
-    >{text}</a>
-  } catch (error) {
-    return <a {...props}>{text}</a>
-  }
-}
+import { useEffect, useRef } from 'react'
+import { marked } from 'marked';
 
 function Markdown({ text, code: style }) {
-  return <div className="markdown">
-    <ReactMarkdown components={{ code: code({ style }), a }}>{text}</ReactMarkdown>
-  </div>
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const html = marked.parse(text)
+    if (ref.current) {
+      ref.current.innerHTML = html
+    }
+  }, [ref])
+
+  return <div ref={ref} className="markdown"></div>
 }
 
 export default Markdown
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,17 @@
 const path = require('path')
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true' && process.env.NODE_ENV === 'production',
+})
+
 const AWS_S3 = `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`
 
-module.exports = {
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: { domains: [AWS_S3] },
   reactStrictMode: true,
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
